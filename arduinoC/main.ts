4e1ade389d7059adf66390dae599a8520e7ee4ef
Vassilis Vallianos <v.vallianos@gmail.com>
2026-03-10 23:55:47 +0200

    Add files via upload
    Now supports strip(s) of 32 leds maximum

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -18,24 +18,36 @@ enum DIGITAL_PORTS {
 //% color="#0D3A2A" iconWidth=50 iconHeight=40
 namespace rgbStrip {
     
-    //% block="Άναψε το RGB Led στο pin [PORT] με χρώμα R: [R], G: [G], B: [B] και φωτεινότητα [BRIGHTNESS]" blockType="command"
+    //% block="Όρισε RGB Strip με [LEDS] led(s) στο pin [PORT]" blockType="command"
+    //% LEDS.shadow="range" LEDS.params.min="1" LEDS.params.max="32" LEDS.defl= "1" 
+	//% PORT.shadow="dropdown" PORT.options="DIGITAL_PORTS" PORT.defl="DIGITAL_PORTS.3"
+    export function setrgbStrip(parameter: any, block: any) {
+        let leds = parameter.LEDS.code;
+		let port = parameter.PORT.code;
+        if(Generator.board === 'arduino'){
+            Generator.addInclude("Adafruit_Neopixel","#include <Adafruit_NeoPixel.h>");
+            Generator.addObject(`Adafruit${port}` ,`Adafruit_NeoPixel`,`RGB_Strip${port}(${leds}, ${port}, NEO_GRB + NEO_KHZ800);`);
+            Generator.addSetup(`neopixel_setup${port}`, `RGB_Strip${port}.begin();`);
+        }
+    }
+	
+	//% block="Άναψε το [LED] RGB Led στο pin [PORT] με χρώμα R: [R], G: [G], B: [B] και φωτεινότητα [BRIGHTNESS]" blockType="command"
+	//% LED.shadow="range" LED.params.min="1" LED.params.max="32" LED.defl= "1"
     //% PORT.shadow="dropdown" PORT.options="DIGITAL_PORTS" PORT.defl="DIGITAL_PORTS.3"
     //% R.shadow="range" R.defl="255" R.params.max="255"
     //% G.shadow="range" G.defl="255" G.params.max="255"
     //% B.shadow="range" B.defl="255" B.params.max="255"
     //% BRIGHTNESS.shadow="range" BRIGHTNESS.defl="128" BRIGHTNESS.params.max="255"
-    export function setrgbStrip(parameter: any, block: any) {
+    export function ledOfStrip(parameter: any, block: any) {
+		let led = parameter.LED.code - 1;
         let port = parameter.PORT.code;
         let r = parameter.R.code;
         let g = parameter.G.code;
         let b = parameter.B.code;
         let brightness = parameter.BRIGHTNESS.code;
         if(Generator.board === 'arduino'){
-            Generator.addInclude("Adafruit_Neopixel","#include <Adafruit_NeoPixel.h>");
-            Generator.addObject(`Adafruit${port}` ,`Adafruit_NeoPixel`,`RGB_Strip${port}(1, ${port}, NEO_GRB + NEO_KHZ800);`);
-            Generator.addSetup(`neopixel_setup${port}`, `RGB_Strip${port}.begin();`);
-			Generator.addCode(`RGB_Strip${port}.setBrightness(${brightness});`);
-            Generator.addCode(`RGB_Strip${port}.setPixelColor(0, RGB_Strip${port}.Color(${r}, ${g}, ${b}));`);
+            Generator.addCode(`RGB_Strip${port}.setBrightness(${brightness});`);
+            Generator.addCode(`RGB_Strip${port}.setPixelColor(${led}, RGB_Strip${port}.Color(${r}, ${g}, ${b}));`);
             Generator.addCode(`RGB_Strip${port}.show();`);
         }
     }
